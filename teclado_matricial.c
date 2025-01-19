071bd3996e9ba3804c9b886e9080205ecbf19b53
Ariagildo L <ariagildo01@gmail.com>
2025-01-19 16:36:30 -0300

    Faz documentação do código

--- teclado_matricial.c
+++ teclado_matricial.c
@@ -15,9 +15,11 @@
 #define TOP 8877
 #define DIV 32
 
+//pinos de linhas e colunas do teclado matricial
 const uint8_t pin_rows[ROWS] = {2, 3, 4, 5};
 const uint8_t pin_columns[COLUMNS] = {16, 17, 18, 19};
 
+//matriz de mapeamento dos botões do teclado matricial
 const char key_map[ROWS][COLUMNS] = {
     {'1', '2', '3', 'A'},
     {'4', '5', '6', 'B'},
@@ -25,6 +27,7 @@ const char key_map[ROWS][COLUMNS] = {
     {'*', '0', '#', 'D'}
 };
 
+//protótipo das funções
 char teclado_get(void);
 void teclado_init (void);
 void led_rgb_init(void);
@@ -35,12 +38,16 @@ void buzzer_init(void);
 int main()
 {
     char tecla;
+    //inicializa os pinos dos periféricos utilizados
     teclado_init();
     led_rgb_init();
     buzzer_init();
+
+    //inicializa comunicação serial
     stdio_init_all();
 
     while (true) {
+        //lê teclado
         tecla = teclado_get();
         switch (tecla)
         {
@@ -95,27 +102,33 @@ int main()
             break;
 
         case 'A':
+            //liga led vermelho
             liga_led_rgb(1,0,0);
             break;
 
         case 'B':
+            //liga led verde
             liga_led_rgb(0,1,0);
             break;
 
         case 'C':
+            //liga led azul
             liga_led_rgb(0,0,1);
             break;
 
         case 'D':
+            //liga todos os leds
             liga_led_rgb(1,1,1);
             break;
 
         case '*':
+            //entra em modo de armazenamento USB
             reset_usb_boot(0,0);
             break;
 
         case '#':
-            pwm_set_chan_level(SLICE_GPIO10, CHANNEL_GPIO10, (TOP + 1)/2);
+            //emite som no buzzer durante 1s
+            pwm_set_chan_level(SLICE_GPIO10, CHANNEL_GPIO10, (TOP + 1)/2); //dutty cycle de 50%
             sleep_ms(1000);
             pwm_set_chan_level(SLICE_GPIO10, CHANNEL_GPIO10, 0);
             break;
@@ -129,13 +142,16 @@ int main()
     }
 }
 
+//função para inicializar os pinos do teclado matricial
 void teclado_init (void){
     int i;
+    //inicializa os pinos de linha do teclado matricial como saída
     for(i = 0; i < ROWS; i++){
         gpio_init(pin_rows[i]);
         gpio_set_dir(pin_rows[i], GPIO_OUT);
     }
 
+    //inicializa os pinos de coluna do teclado matricial como entrada
     for(i = 0; i < COLUMNS; i++){
         gpio_init(pin_columns[i]);
         gpio_set_dir(pin_columns[i], GPIO_IN);
@@ -144,11 +160,15 @@ void teclado_init (void){
 
 }
 
+//função para ler tecla pressionada
 char teclado_get(void){
     int r, c;
     for (r = 0; r < ROWS; r++){
+        //coloca sinal de tensão alto nos pinos de linha do teclado
         gpio_put(pin_rows[r], true);
+        //varre as colunas de 1 a 4 do teclado testando se algum botão na linha atual foi precionado
         for (c = 0; c < COLUMNS; c++){
+            //se precionado retorna o caractere correspondente ao botão
             if(gpio_get(pin_columns[c])){
                 gpio_put(pin_rows[r], false);
                 return key_map[r][c];
@@ -156,9 +176,11 @@ char teclado_get(void){
         }
         gpio_put(pin_rows[r], false);
     }
+    //se nenhum botão for precionado retorna caractere nulo
     return '\0';
 }
 
+//função para inicializar os pinos do led rgb
 void led_rgb_init(void){
     gpio_init(LED_RED);
     gpio_init(LED_GREEN);
@@ -169,6 +191,7 @@ void led_rgb_init(void){
     gpio_set_dir(LED_BLUE, GPIO_OUT);
 }
 
+//função para ligar leds selecionados por 1s
 void liga_led_rgb(bool red, bool green, bool blue){
     gpio_put(LED_RED, red);
     gpio_put(LED_GREEN, green);
@@ -179,10 +202,23 @@ void liga_led_rgb(bool red, bool green, bool blue){
     gpio_put(LED_BLUE, 0);
 }
 
+//função para inicializar o pwm do buzzer
 void buzzer_init(void){
+    //inicializa o pino do buzzer como pwm
     gpio_set_function(BUZZER, GPIO_FUNC_PWM);
+    //seta valor máximo de contagem do PWM (TOP)
     pwm_set_wrap(SLICE_GPIO10, TOP);
+    //seta valor de divisão do clock do PWM
     pwm_set_clkdiv(SLICE_GPIO10, DIV);
+
+    /*
+    valor de TOP e DIV previamente calculado para emitir som da nota LÁ 4 (440 Hz): 
+    f_pwm = f_sys /((TOP + 1) * (DIV) * (CSR_PH_CORRECT + 1)) ~ 440 
+    .: CSR_PH_CORRECT não setado -> 0
+    */
+
+    //seta valor inicial de dutty cycle como 0
     pwm_set_chan_level(SLICE_GPIO10, CHANNEL_GPIO10, 0);
+    //abilita PWM
     pwm_set_enabled(SLICE_GPIO10, true);
 }
