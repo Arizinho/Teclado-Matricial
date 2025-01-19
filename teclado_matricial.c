cbde68c764d6fe36e1ef7ca62f7d809b02b4c11d
Ariagildo L <ariagildo01@gmail.com>
2025-01-19 15:23:05 -0300

    Adiciona definição de pinos utilizados

--- teclado_matricial.c
+++ teclado_matricial.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
 
+#define ROWS 4
+#define COLUMNS 4
+#define LED_RED 12
+#define LED_GREEN 13
+#define LED_BLUE 14
+#define BUZZER 10
+
+const uint8_t pin_rows[ROWS] = {2, 3, 4, 5};
+const uint8_t pin_columns[COLUMNS] = {16, 17, 18, 19};
 
 
 int main()
